474453bd5862f48eb3846dc540d378f1a28957ea
mizuki-takano <mizuki.takano@prum.jp>
2026-08-16 16:01:07 +0900

    ログイン時のメールアドレス認証の設定

--- src/main/java/com/spring/springbootapplication/LoginForm.java
+++ src/main/java/com/spring/springbootapplication/LoginForm.java
@@ -1,29 +1,23 @@
 package com.spring.springbootapplication;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
-/**
- * LoginForm
- */
+
+@Data
 public class LoginForm {
 
+    @NotBlank(message = "メールアドレスは必ず入力してください。")
+    @Pattern(
+        regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z]{2,}$",
+        message = "メールアドレスが正しい形式ではありません。")
     private String email;
 
+    @NotBlank(message = "パスワードは必ず入力してください。")
+    @Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+        message = "英数字8文字以上で入力してください。")
     private String password;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    
 }
