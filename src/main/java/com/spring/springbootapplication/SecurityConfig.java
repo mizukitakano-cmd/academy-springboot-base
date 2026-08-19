803cb44124214c74e4d958f9c66a567839d5a795
mizuki-takano <mizuki.takano@prum.jp>
2026-08-19 14:38:29 +0900

    SecurityConfig.javaの修正

--- src/main/java/com/spring/springbootapplication/SecurityConfig.java
+++ src/main/java/com/spring/springbootapplication/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception{
         http
             .authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/login", "/signin", "/signup", "/css/**", "/js/**").permitAll()
+            .requestMatchers("/login", "/signin", "/signup", "/register", "/css/**", "/js/**").permitAll()
             .anyRequest().authenticated()
         )
 
