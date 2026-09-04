2c38a9beb5aa4748ba154a12c0f443606f324392
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 14:04:51 +0900

    applicationとdata.sqlの修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
-TRUNCATE TABLE users CASCADE;
+DELETE FROM users;
 
 INSERT INTO users (name, email, password)
-VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW');
+SELECT 'kinako', 'kinako@mail.com' '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW'
+WHERE NOT EXISTS (
+    SELECT 1 FROM users WHERE email = 'kinako@mail.com'
+);
