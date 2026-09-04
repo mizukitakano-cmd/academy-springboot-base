f1bbc06818575ee507d641500805a7f197f90b93
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 16:18:15 +0900

    簡易TOPに遷移しないエラー解消

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 TRUNCATE TABLE users RESTART IDENTITY CASCADE;
 
-INSERT INTO users (name, email, password) VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW') NO CONFLICT DO NOTHING;
-INSERT INTO "Users" (name, email, password) VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW') NO CONFLICT DO NOTHING;
+INSERT INTO users (name, email, password)
+VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW');
+
