f404301c330d1bf1da7709c8b0307c7b65473d39
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 19:22:30 +0900

    data.sqlの階層を変更とファイル名をV1_insert_user.sqlに変更

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE users RESTART IDENTITY CASCADE;
-
-INSERT INTO users (name, email, password)
-VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW');
-
