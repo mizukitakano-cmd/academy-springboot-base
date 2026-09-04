0accff3d057d76f67398b7971bd1520d91c2205a
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 14:57:41 +0900

    遷移しないエラー解消

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-DELETE FROM users;
-
-INSERT INTO "Users" (name, email, password)
-SELECT 'kinako', 'kinako@mail.com' '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW'
-WHERE NOT EXISTS (
-    SELECT 1 FROM users WHERE email = 'kinako@mail.com'
-);
+INSERT INTO users (name, email, password) VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW') NO CONFLICT DO NOTHING;
+INSERT INTO "Users" (name, email, password) VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW') NO CONFLICT DO NOTHING;
