5081f79fcbe297d47fdb18a7ddb967104a91d490
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 13:39:01 +0900

    SQL書き換え

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 TRUNCATE TABLE users CASCADE;
 
-INSERT INTO users (id, name, email, password)
-VALUES (10, 'kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW');
+INSERT INTO users (name, email, password)
+VALUES ('kinako', 'kinako@mail.com', '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW');
