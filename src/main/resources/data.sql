30436fb7fdacacfbf00b75747f82fe0d9b77424b
mizuki-takano <mizuki.takano@prum.jp>
2026-09-04 14:21:26 +0900

    タイポ修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 DELETE FROM users;
 
-INSERT INTO users (name, email, password)
+INSERT INTO "Users" (name, email, password)
 SELECT 'kinako', 'kinako@mail.com' '$2a$10$0p2P6Ushm087W7xQ3A25Mep2fscbV2Xq.pAen1090huasw7Z6tGOW'
 WHERE NOT EXISTS (
     SELECT 1 FROM users WHERE email = 'kinako@mail.com'
